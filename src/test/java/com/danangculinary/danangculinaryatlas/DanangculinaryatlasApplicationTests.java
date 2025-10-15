8cec0bce98169019b8935a2bde74d1dd26736aee
givoxxs <toanyogame@gmail.com>
2025-10-15 23:10:15 +0700

    fix: update SpringBootTest annotation to use the correct application class for testing

--- src/test/java/com/danangculinary/danangculinaryatlas/DanangculinaryatlasApplicationTests.java
+++ src/test/java/com/danangculinary/danangculinaryatlas/DanangculinaryatlasApplicationTests.java
@@ -3,11 +3,15 @@ package com.danangculinary.danangculinaryatlas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DanangculinaryatlasApplication.class)
+// Import ĐÚNG class application chính từ ĐÚNG package của nó
+import com.atlasculinary.AtlasCulinaryBackendApplication;
+
+// Chỉ định rõ class cấu hình cần load cho bài test
+@SpringBootTest(classes = AtlasCulinaryBackendApplication.class)
 class DanangculinaryatlasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
