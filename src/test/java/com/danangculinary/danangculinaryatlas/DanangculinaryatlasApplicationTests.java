bf92fe41ff960162da9fee8d74735c233e29e5fe
givoxxs <toanyogame@gmail.com>
2025-10-15 23:03:53 +0700

    fix: specify application class in SpringBootTest annotation for improved test context

--- src/test/java/com/danangculinary/danangculinaryatlas/DanangculinaryatlasApplicationTests.java
+++ src/test/java/com/danangculinary/danangculinaryatlas/DanangculinaryatlasApplicationTests.java
@@ -3,7 +3,7 @@ package com.danangculinary.danangculinaryatlas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DanangculinaryatlasApplication.class)
 class DanangculinaryatlasApplicationTests {
 
 	@Test
